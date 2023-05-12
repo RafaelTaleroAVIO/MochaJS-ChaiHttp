ad5a1b24a3baa88a98eae02e9e3d01fca2289cf9
Joan González <joan.gonzalez@ac0106.local>
2023-05-12 14:50:24 -0500

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MochaJS-ChaiHttp
+
 Offsite AVIO API framework base
+
+# Steps to launch test:
+
+- Run tests: `npm run test` or `npm test`
+
+# Get report:
+
+Run test and then go to mochawesome-report folder, and check mochawesome.html
--- test/Example.js
+++ test/Example.js
@@ -1,97 +1,98 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const expect = chai.expect;
 const assert = chai.assert;
-require('dotenv').config();
+require("dotenv").config();
 
 chai.use(chaiHttp);
 
-describe('GET requests', function() {
-    it('should get the list of all the users', function(done) {
-    chai.request(process.env.URL)
-    .get("/api/users?page=2")
-    .end(function(err, res) {
-        expect(res).to.have.status(200,"Status code different from 200");
-        expect(res.body.data).to.be.an('array');
+describe("GET requests", function () {
+  it("should get the list of all the users", function (done) {
+    chai
+      .request(process.env.URL)
+      .get("/api/users?page=2")
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body.data).to.be.an("array");
         expect(res.body.page).to.be.equal(2);
         expect(res.body.lenght).to.be.not.null;
-        done();  
-    });
-    });
+        done();
+      });
+  });
 
-    it('should get the info of a single user', function(done) {
-    chai.request(process.env.URL)
-    .get("/api/users/2")
-    .end(function(err, res) {
-        expect(res).to.have.status(200,"Status code different from 200");
+  it("should get the info of a single user", function (done) {
+    chai
+      .request(process.env.URL)
+      .get("/api/users/2")
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
         expect(res.body.lenght).to.be.not.null;
-        done();  
-    });
-    });
+        done();
+      });
+  });
 
-    it('verify if user was not found', function(done) {
-    chai.request(process.env.URL)
-    .get("/api/users/23")
-    .end(function(err, res) {
+  it("verify if user was not found", function (done) {
+    chai
+      .request(process.env.URL)
+      .get("/api/users/23")
+      .end(function (err, res) {
         expect(res).to.have.status(404);
         expect(res.body).to.be.empty;
-        done();  
-    });
-    });
+        done();
+      });
+  });
 
-    it('verify all the color are displayed', function(done) {
-    chai.request(process.env.URL)
-    .get("/api/unknown")
-    .end(function(err, res) {
+  it("verify all the color are displayed", function (done) {
+    chai
+      .request(process.env.URL)
+      .get("/api/unknown")
+      .end(function (err, res) {
         expect(res).to.have.status(200);
         assert.lengthOf(res.body.data, 6);
-        done();  
-    });
-    });
+        done();
+      });
+  });
 });
 
-describe('POST requests', function() {
-    it('Create a single user', function(done) {
-        const name_ = 'John';
-        const job_ = 'Automation';
-        chai.request(process.env.URL)
-        .post("/api/users")
-        .send({
-            name: name_, job: job_
-        })
-        .end(function(err, res){
-            expect(res).to.has.status(201);
-            expect(res.body.name).to.equal(name_);
-            expect(res.body.job).to.equal(job_);
-            assert.isString(res.body.id);
-            assert.match(res.body.id, /^[0-9]+$/);
-            assert.isNotEmpty(res.body.createdAt);
-            done();
-        });
-    })
-})
-
-  describe('GET requests', function() {
-    it('Delayed call', function() {
-    this.timeout(6000);
-      return new Promise(function(resolve, reject) {
-        chai.request(process.env.URL)
-          .get(process.env.API_PATH)
-          .end(function(err, res){
-            if (res.status === 200) {
-                expect(res.body.data).to.be.an('array');
-                expect(res.body.page).to.be.equal(1);
-              resolve();
-            } else {
-              reject(new Error("Not getting 200 status API response"));
-            }
-          });
+describe("POST requests", function () {
+  it("Create a single user", function (done) {
+    const name_ = "John";
+    const job_ = "Automation";
+    chai
+      .request(process.env.URL)
+      .post("/api/users")
+      .send({
+        name: name_,
+        job: job_,
+      })
+      .end(function (err, res) {
+        expect(res).to.has.status(201);
+        expect(res.body.name).to.equal(name_);
+        expect(res.body.job).to.equal(job_);
+        assert.isString(res.body.id);
+        assert.match(res.body.id, /^[0-9]+$/);
+        assert.isNotEmpty(res.body.createdAt);
+        done();
       });
+  });
+});
+
+describe("GET requests", function () {
+  it("Delayed call", function () {
+    this.timeout(6000);
+    return new Promise(function (resolve, reject) {
+      chai
+        .request(process.env.URL)
+        .get(process.env.API_PATH || "")
+        .end(function (err, res) {
+          if (res.status === 200) {
+            expect(res.body.data).to.be.an("array");
+            expect(res.body.page).to.be.equal(1);
+            resolve("resolved");
+          } else {
+            reject(new Error("Not getting 200 status API response"));
+          }
+        });
     });
   });
-  
-  
-  
-  
-  
-  
+});
